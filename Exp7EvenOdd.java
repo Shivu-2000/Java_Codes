eac99b9d8a78d64778c8393d64b9ada8a9e4e02f
SHIVANI GUPTA <100036984+Shivu-2000@users.noreply.github.com>
2022-12-01 13:31:46 +0530

    Program 7

---
+++ Exp7EvenOdd.java
@@ -0,0 +1,13 @@
+//Program to check the num is even or odd
+import java.lang.*;
+class Exp7EvenOdd
+{
+	public static void main(String arg[])
+	{
+		int a=19;
+		if(a%2==0)
+			System.out.println("Number is even");
+		else
+			System.out.println("Number is odd");
+	}
+}
